c83df54a8dc8b05662edd74d4f714fb687f1e184
Richard Massey <masseyr44@gmail.com>
2018-07-04 12:46:22 -0700

    updated imports

--- scripts/modules/__init__.py
+++ scripts/modules/__init__.py
@@ -3,4 +3,5 @@ from plots import Plot
 from classification import RFRegressor, MRegressor, Samples
 from raster import Raster
 from resources import *
+from distance import Mahalanobis, Distance
 from exceptions import ObjectNotFound, UninitializedError
