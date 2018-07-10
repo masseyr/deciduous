b6c9e263ac4ba57c6e90f1a99e0e03bdc38d2ae0
Richard Massey <masseyr44@gmail.com>
2018-07-10 13:24:36 -0700

    Script for cleaning sample data

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -1,16 +1,19 @@
 from modules import *
 import numpy as np
 import pandas as pd
+import random
 
 if __name__ == '__main__':
 
     # static vars--------------------------------------------------------------------------------------------
-    md_cutoff = 75
+    md_cutoff = 95
+    ratio = 0.95
 
-    outdir = "c:\\users\\rm885\\Dropbox\\projects\\NAU\\landsat_deciduous\\data\\"
+    outdir = "C:\\users\\rm885\\Dropbox\\projects\\NAU\\landsat_deciduous\\data\\"
 
-    infile = outdir + "ABoVE_all_2010_sampV1.csv"
-    outfile = outdir + "ABoVE_all_2010_sampV1_clean_md{}.csv".format(str(md_cutoff))
+    infile = outdir + "ABoVE_CAN_AK_all_2010_samp.csv"
+    trn_outfile = outdir + "ABoVE_CAN_AK_all_2010_trn_samp_clean_md{}.csv".format(str(md_cutoff))
+    val_outfile = outdir + "ABoVE_CAN_AK_all_2010_val_samp_clean_md{}.csv".format(str(md_cutoff))
 
     header = ['site',
               'sample']
@@ -27,13 +30,27 @@ if __name__ == '__main__':
     for name in names[1:-1]:
         header.append(name)
 
+    print(header)
+
     site_data = list()
 
     # convert strings like '1_125_3' into sites and samples
     for elem in data:
         index = elem[0].split('_')
-        site_id = int(index[0])*1000 + int(index[1])
-        samp_id = int(index[2])
+
+        if len(index) == 3:
+
+            site_id = int(index[0])*10000 + int(index[1])
+            samp_id = int(index[2])
+
+        elif len(index) == 4:
+
+            site_id = int(index[0])*10000 + int(index[1])*1000 * int(index[2])
+            samp_id = int(index[3])
+
+        else:
+            print('Invalid Sample ID: {}'.format(elem))
+            continue
 
         vals = dict()
 
@@ -45,52 +62,143 @@ if __name__ == '__main__':
 
         site_data.append(vals)
 
+    # for data in site_data:
+    #     print(data)
+
     data = None
-    out_data = list()
+    trn_data = list()
 
     # list of unique sites
     sites = np.unique([elem['site'] for elem in site_data])
 
+    print('Total {} sites'.format(str(len(sites))))
+
+    ntrn = int(ratio * len(sites))
+    nval = len(sites) - ntrn
+
+    trn_sites = random.sample(sites, ntrn)
+    val_sites = Sublist(sites).remove(trn_sites)
+
+    print(trn_sites)
+    print(len(trn_sites))
+
+    print('-------------------------')
+
+    print(val_sites)
+    print(len(val_sites))
+
+    print('-------------------------')
+
     # loop through all sites
-    for site in sites:
+    for site in trn_sites:
 
         # collect samples for the site
         samples = list(elem for elem in site_data if elem['site'] == site)
 
-        # initialize mahalanobis class
-        md_data = Mahalanobis(samples=samples,
-                              names=bandnames)
+        """
+        print('-----------------------------------------------------------')
+        print(site)
+        for sample in samples:
+            print(sample)
+        """
 
-        md_data.sample_matrix()
-        md_data.cluster_center(method='median')
-        md_vec = md_data.calc_distance()
+        if len(samples) > len(bandnames):
 
-        # eliminate NaNs in Mahalanobis dist vector
-        num_list = list(i for i, x in enumerate(list(np.isnan(md_vec))) if not x)
-        if len(num_list) < 1:
-            continue
+            # initialize mahalanobis class
+            md_data = Mahalanobis(samples=samples,
+                                  names=bandnames)
 
-        md_vec = [md_vec[x] for x in num_list]
+            md_data.sample_matrix()
+            md_data.cluster_center(method='median')
+            md_vec = md_data.calc_distance()
 
-        # find all MD values that as less than cutoff percentile
-        loc = list(i for i, x in enumerate(md_vec) if (x <= np.percentile(md_vec, md_cutoff) and x != np.nan))
+            # eliminate NaNs in Mahalanobis dist vector
+            num_list = list(i for i, x in enumerate(list(np.isnan(md_vec))) if not x)
+            if len(num_list) < 1:
+                continue
 
-        out_samples = list(samples[i] for i in loc)
+            md_vec = [md_vec[x] for x in num_list]
+
+            # find all MD values that as less than cutoff percentile
+            loc = list(i for i, x in enumerate(md_vec) if (x <= np.percentile(md_vec, md_cutoff) and x != np.nan))
+
+            out_samples = list(samples[i] for i in loc)
+
+        else:
+
+            print('Too few samples for cleaning')
+
+            out_samples = samples
 
         # add all filtered samples to out list
         for sample in out_samples:
-            out_data.append(sample)
+            trn_data.append(sample)
 
-    print(len(out_data))
+    print('\n\nTotal samples: {}'.format(str(len(trn_data))))
+
+    val_data = list()
+
+    for site in val_sites:
+
+        samples = list(elem for elem in site_data if elem['site'] == site)
+
+        if len(samples) > len(bandnames):
+
+            # initialize mahalanobis class
+            md_data = Mahalanobis(samples=samples,
+                                  names=bandnames)
+
+            md_data.sample_matrix()
+            md_data.cluster_center(method='median')
+            md_vec = md_data.calc_distance()
+
+            # eliminate NaNs in Mahalanobis dist vector
+            num_list = list(i for i, x in enumerate(list(np.isnan(md_vec))) if not x)
+            if len(num_list) < 1:
+                continue
+
+            md_vec = [md_vec[x] for x in num_list]
+
+            # find all MD values that as less than cutoff percentile
+            loc = list(i for i, x in enumerate(md_vec) if (x <= np.percentile(md_vec, md_cutoff) and x != np.nan))
+
+            out_samples = list(samples[i] for i in loc)
+
+        else:
+
+            print('Too few samples for cleaning')
+
+            out_samples = samples
+
+        # val_samp = dict(site=samples[0]['site'], decid_frac=samples[0]['decid_frac'])
+
+        for sample in out_samples:
+            val_data.append(sample)
+
+        # val_data.append(samples[0])
 
     # Make a pandas dataframe
-    df = pd.DataFrame(out_data)
+    t_df = pd.DataFrame(trn_data)
+    v_df = pd.DataFrame(val_data)
 
     # remove useless columns
-    df.drop(columns=['geo', 'site', 'sample',
-                     'RATIO57', 'RATIO57_1', 'RATIO57_2'],
-            inplace=True)
+    t_df.drop(columns=['geo', 'site', 'sample',
+                       'BLUE', 'BLUE_1', 'BLUE_2',
+                     'GREEN', 'GREEN_1', 'GREEN_2',
+                     'RED', 'RED_1', 'RED_2',
+                     'SWIR2', 'SWIR2_1', 'SWIR2_2',
+                       "EVI", "EVI_1", "EVI_2"],
+              inplace=True)
 
+    v_df.drop(columns=['geo', 'site', 'sample',
+                       'BLUE', 'BLUE_1', 'BLUE_2',
+                     'GREEN', 'GREEN_1', 'GREEN_2',
+                     'RED', 'RED_1', 'RED_2',
+                     'SWIR2', 'SWIR2_1', 'SWIR2_2',
+                       "EVI", "EVI_1", "EVI_2"],
+              inplace=True)
+
+    """
     # muliply some columns with 10000
     multiply_cols = ['BLUE', 'BLUE_1', 'BLUE_2',
                      'GREEN', 'GREEN_1', 'GREEN_2',
@@ -101,7 +209,11 @@ if __name__ == '__main__':
 
     for i in range(0, len(multiply_cols)):
         df[multiply_cols[i]] = df[multiply_cols[i]]*10000
+    """
 
     # write to csv file
-    df.to_csv(outfile,
-              index=False)
+    t_df.to_csv(trn_outfile,
+                index=False)
+
+    v_df.to_csv(val_outfile,
+                index=False)
