91171a2c1e4ec26a3674bffbdeaac9fca8a8ea63
Richard Massey <masseyr44@gmail.com>
2018-10-25 21:42:00 -0700

    import timer

--- modules/__init__.py
+++ modules/__init__.py
@@ -4,6 +4,7 @@ from classification import RFRegressor, MRegressor
 from samples import Samples
 from raster import Raster
 from resources import *
+from timer import Timer
 from distance import Mahalanobis, Distance
 from exceptions import ObjectNotFound, UninitializedError
 from vector import Vector, OGR_GEOM_DEF, OGR_TYPE_DEF, OGR_FIELD_DEF
