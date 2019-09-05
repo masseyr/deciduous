ff1cdcd6208d04558896f65d92b7408bfda1f168
Richard Massey <rm885@nau.edu>
2019-09-05 14:26:24 -0700

    updated ___init__

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,11 +1,11 @@
 from common import Sublist, Handler, FTPHandler, Opt, \
     OGR_GEOM_DEF, OGR_TYPE_DEF, OGR_FIELD_DEF, GDAL_FIELD_DEF, OGR_FIELD_DEF_INV, GDAL_FIELD_DEF_INV
-from classification import RFRegressor, MRegressor, HRFRegressor
+from classification import RFRegressor, MRegressor, HRFRegressor, _Regressor
 from samples import Samples
 from raster import Raster
 from resources import *
 from timer import Timer
-from distance import Mahalanobis, Distance
+from distance import Mahalanobis, Distance, Euclidean
 from exceptions import ObjectNotFound, UninitializedError
 from vector import Vector
 from logger import Logger
