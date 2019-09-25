c269d18231068afcc21ae5ce9c4b5e7945f76546
Richard <masseyr44@gmail.com>
2019-09-25 16:49:42 -0700

    updated data/rasterize_shapefile.py

--- data/rasterize_shapefile.py
+++ data/rasterize_shapefile.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
-    import ee
+
     import sys
     import os
 
@@ -7,12 +7,21 @@ if __name__ == '__main__':
     sys.path.append(module_path)
     from modules import Vector
 
-    infile = "C:/temp/arctic_oroarctic_wgs84_buf50km.shp"
-    outfile = "C:/temp/arctic_oroarctic_wgs84_buf50km.tif"
+    infile = "C:/temp/arctic_oroarctic_wgs84.shp"
+    outshpfile = "C:/temp/arctic_oroarctic_wgs84_2km.shp"
+    outfile = "C:/temp/arctic_oroarctic_wgs84_2km.tif"
+
+    buf_size = 0.0168
     pixel_size = (0.0168, 0.0168)
 
     vec = Vector(infile)
 
     print(vec)
 
-    vec.rasterize(outfile, pixel_size, 1, 0, None)
+    vec_buf = vec.buffer(buf_size, return_vector=True)
+
+    print(vec_buf)
+
+    vec_buf.write_vector(outshpfile)
+
+    vec_buf.rasterize(outfile, pixel_size, 1, 0, None)
