800a6475aaf6f436c15fe57c7bd4e19fa0c7afa7
Richard <masseyr44@gmail.com>
2019-10-04 16:49:41 -0700

    updated data/mosaic_files.py

---
+++ data/mosaic_files.py
@@ -0,0 +1,21 @@
+from geosoup import MultiRaster
+
+
+if __name__ == '__main__':
+
+    files = ["D:/temp/decid/decid_change_2000_2015_f3-0000023296-0000046592.tif",
+             "D:/temp/decid/decid_change_2000_2015_f3-0000000000-0000023296.tif",
+             "D:/temp/decid/decid_change_2000_2015_f3-0000000000-0000000000.tif",
+             "D:/temp/decid/decid_change_2000_2015_f3-0000000000-0000046592.tif",
+             "D:/temp/decid/decid_change_2000_2015_f3-0000023296-0000000000.tif",
+             "D:/temp/decid/decid_change_2000_2015_f3-0000023296-0000023296.tif"]
+
+    outfile = "D:/temp/decid/decid_change_2000_2015_f3.tif"
+
+    mras = MultiRaster(files)
+
+    mras.mosaic(add_overviews=True,
+                outfile=outfile,
+                nodata_values=None,
+                compress='LZW',
+                bigtiff='YES')
