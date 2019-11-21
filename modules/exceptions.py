f6b576efbebf5d60a54f530a1d5c2e1ad906e051
Richard Massey <masseyr44@gmail.com>
2019-11-21 15:16:11 -0700

    updated exceptions.py

--- modules/exceptions.py
+++ modules/exceptions.py
@@ -1,5 +1,9 @@
 __all__ = ['UninitializedError',
-           'ObjectNotFound']
+           'ObjectNotFound',
+           'ImageProcessingError',
+           'TileNotFound',
+           'FileNotFound',
+           'FieldError']
 
 
 class UninitializedError(Exception):
@@ -33,6 +37,90 @@ class ObjectNotFound(Exception):
 
     def __init__(self, error=None,
                  description="Object not found",
+                 status_code=status_code,
+                 headers=None):
+        """
+        :param error: name of error
+        :param description: readable description
+        :param status_code: the http status code
+        :param headers: any applicable headers
+        :return:
+        """
+        self.description = description
+        self.status_code = status_code
+        self.headers = headers
+        self.error = error
+
+
+class FieldError(Exception):
+    """
+    Error class for use in returning fields that may be in error.
+    """
+    status_code = 200
+
+    def __init__(self,
+                 error="Bad Request",
+                 description="Missing or invalid information",
+                 status_code=status_code,
+                 headers=None):
+        """
+        :param error: name of error
+        :param description: readable description
+        :param status_code: the http status code
+        :param headers: any applicable headers
+        :return:
+        """
+        self.description = description
+        self.status_code = status_code
+        self.headers = headers
+        self.error = error
+
+
+class ImageProcessingError(Exception):
+    status_code = 500
+
+    def __init__(self, error="Image Processing Error",
+                 description="Something went wrong processing your image",
+                 status_code=status_code,
+                 headers=None):
+        """
+        :param error: name of error
+        :param description: readable description
+        :param status_code: the http status code
+        :param headers: any applicable headers
+        :return:
+        """
+        self.description = description
+        self.status_code = status_code
+        self.headers = headers
+        self.error = error
+
+
+class TileNotFound(Exception):
+    status_code = 601
+
+    def __init__(self, error="Image Processing Error",
+                 description="Image tile not found",
+                 status_code=status_code,
+                 headers=None):
+        """
+        :param error: name of error
+        :param description: readable description
+        :param status_code: the http status code
+        :param headers: any applicable headers
+        :return:
+        """
+        self.description = description
+        self.status_code = status_code
+        self.headers = headers
+        self.error = error
+
+
+class FileNotFound(Exception):
+    status_code = 600
+
+    def __init__(self, error=None,
+                 description="No matching file found on disk",
                  status_code=status_code,
                  headers=None):
         """
