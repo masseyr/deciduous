72ac30b469b0bdaee6390174d871511585730c70
Richard Massey <rm885@nau.edu>
2020-02-09 15:02:05 -0700

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,4 +1,4 @@
-from common import Sublist, Handler, FTPHandler, Opt, \
+from common import Sublist, Handler, FTPHandler, Opt, EEFunc, \
     OGR_GEOM_DEF, OGR_TYPE_DEF, OGR_FIELD_DEF, GDAL_FIELD_DEF, OGR_FIELD_DEF_INV, GDAL_FIELD_DEF_INV
 from classification import RFRegressor, MRegressor, HRFRegressor, _Regressor
 from samples import Samples
