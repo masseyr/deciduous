e7b4265affd872b1e2b084b8ec82819700d7a543
Richard Massey <rm885@nau.edu>
2020-02-10 00:49:03 -0700

    Update data_prep_boreal_extent_GEE.py

--- data/data_prep_boreal_extent_GEE.py
+++ data/data_prep_boreal_extent_GEE.py
@@ -5,7 +5,6 @@ for deciduous fraction and tree cover regression
 
 if __name__ == '__main__':
     import ee
-    import math
     import sys
     import os
 
@@ -15,134 +14,15 @@ if __name__ == '__main__':
 
     ee.Initialize()
 
-    # ------------------functions -----------------------------------------------------------------------------------
-    def ndvi_calc(img, scale_factor=10000):
-        """ Normalized difference vegetation index"""
-        return img.normalizedDifference(['NIR', 'RED']).select([0], ['NDVI']).multiply(scale_factor).toInt16()
-
-    def vari_calc(img, scale_factor=10000):
-        """ Visible Atmospherically Resistant Index"""
-        return (img.select(['RED']).subtract(img.select(['GREEN'])))\
-            .divide(img.select(['RED']).add(img.select(['GREEN'])).subtract(img.select(['BLUE'])))\
-            .select([0], ['VARI']).multiply(scale_factor).toInt16()
-
-    def ndwi_calc(img, scale_factor=10000):
-        """ Normalized difference wetness index"""
-        return img.normalizedDifference(['NIR', 'SWIR2']).select([0], ['NDWI']).multiply(scale_factor).toInt16()
-
-    def nbr_calc(img, scale_factor=10000):
-        """ Normalized burn ratio"""
-        return img.normalizedDifference(['NIR', 'SWIR1']).select([0], ['NBR']).multiply(scale_factor).toInt16()
-
-    def savi_calc(img, const=0.5, scale_factor=10000):
-        """ Soil adjusted vegetation index"""
-        return (img.select(['NIR']).subtract(img.select(['RED'])).multiply(1 + const))\
-            .divide(img.select(['NIR']).add(img.select(['RED'])).add(const))\
-            .select([0], ['SAVI']).multiply(scale_factor).toInt16()
-
-    def add_indices(in_image, const=0.5, scale_factor=10000):
-        """ Function to add indices to an image:  NDVI, NDWI, VARI, NBR, SAVI"""
-        temp_image = in_image.float().divide(scale_factor)
-        return in_image.addBands(ndvi_calc(temp_image, scale_factor))\
-            .addBands(ndwi_calc(temp_image, scale_factor))\
-            .addBands(vari_calc(temp_image, scale_factor))\
-            .addBands(nbr_calc(temp_image, scale_factor))\
-            .addBands(savi_calc(temp_image, const, scale_factor))
-
-    def add_suffix(in_image, suffix_str):
-        """ Add suffix to all band names"""
-        bandnames = in_image.bandNames().map(lambda elem: ee.String(elem).toLowerCase().cat('_').cat(suffix_str))
-        nb = bandnames.length()
-        return in_image.select(ee.List.sequence(0, ee.Number(nb).subtract(1)), bandnames)
-
-    def ls8_sr_corr(img):
-        """ Method to correct Landsat 8 based on Landsat 7 reflectance.
-            This method scales the SR reflectance values to match LS7 reflectance
-            The returned values are generally lower than input image
-            based on roy et al 2016"""
-        return img.select(['B2'], ['BLUE']).float().multiply(0.8850).add(183).int16()\
-            .addBands(img.select(['B3'], ['GREEN']).float().multiply(0.9317).add(123).int16())\
-            .addBands(img.select(['B4'], ['RED']).float().multiply(0.9372).add(123).int16())\
-            .addBands(img.select(['B5'], ['NIR']).float().multiply(0.8339).add(448).int16())\
-            .addBands(img.select(['B6'], ['SWIR1']).float().multiply(0.8639).add(306).int16())\
-            .addBands(img.select(['B7'], ['SWIR2']).float().multiply(0.9165).add(116).int16())\
-            .addBands(img.select(['pixel_qa'], ['PIXEL_QA']).int16())\
-            .addBands(img.select(['radsat_qa'], ['RADSAT_QA']).int16())\
-            .copyProperties(img)\
-            .copyProperties(img, ['system:time_start', 'system:time_end', 'system:index', 'system:footprint'])
-
-    def ls5_sr_corr(img):
-        """ Method to correct Landsat 5 based on Landsat 7 reflectance.
-            This method scales the SR reflectance values to match LS7 reflectance
-            The returned values are generally lower than input image
-            based on sulla-menashe et al 2016"""
-        return img.select(['B1'], ['BLUE']).float().multiply(0.91996).add(37).int16()\
-            .addBands(img.select(['B2'], ['GREEN']).float().multiply(0.92764).add(84).int16())\
-            .addBands(img.select(['B3'], ['RED']).float().multiply(0.8881).add(98).int16())\
-            .addBands(img.select(['B4'], ['NIR']).float().multiply(0.95057).add(38).int16())\
-            .addBands(img.select(['B5'], ['SWIR1']).float().multiply(0.96525).add(29).int16())\
-            .addBands(img.select(['B7'], ['SWIR2']).float().multiply(0.99601).add(20).int16())\
-            .addBands(img.select(['pixel_qa'], ['PIXEL_QA']).int16())\
-            .addBands(img.select(['radsat_qa'], ['RADSAT_QA']).int16())\
-            .copyProperties(img)\
-            .copyProperties(img, ['system:time_start', 'system:time_end', 'system:index', 'system:footprint'])
-
-    def ls_sr_band_correction(img):
-        """ This method renames LS5, LS7, and LS8 bands and corrects LS5 and LS8 bands
-            this method should be used with SR only"""
-        return \
-            ee.Algorithms.If(
-                ee.String(img.get('SATELLITE')).compareTo('LANDSAT_8'),
-                ee.Algorithms.If(
-                    ee.String(img.get('SATELLITE')).compareTo('LANDSAT_5'),
-                    ee.Image(img.select(['B1', 'B2', 'B3', 'B4', 'B5', 'B7', 'pixel_qa', 'radsat_qa'],
-                                        ['BLUE', 'GREEN', 'RED', 'NIR', 'SWIR1', 'SWIR2', 'PIXEL_QA', 'RADSAT_QA'])
-                             .int16()
-                             .copyProperties(img)
-                             .copyProperties(img,
-                                             ['system:time_start',
-                                              'system:time_end',
-                                              'system:index',
-                                              'system:footprint'])),
-                    ee.Image(ls5_sr_corr(img))
-                ),
-                ee.Image(ls8_sr_corr(img))
-            )
-
-    def ls_sr_only_clear(image):
-        """ Method to calcluate clear mask based on pixel_qa and radsat_qa bands"""
-        clearbit = 1
-        clearmask = math.pow(2, clearbit)
-        qa = image.select('PIXEL_QA')
-        qa_mask = qa.bitwiseAnd(clearmask)
-
-        ra = image.select('RADSAT_QA')
-        ra_mask = ra.eq(0)
-
-        return ee.Image(image.updateMask(qa_mask).updateMask(ra_mask))
-
     def get_landsat_images(collection, bounds, start_date, end_date, start_julian, end_julian):
         """ Make collections based on given parameters"""
         return ee.ImageCollection(collection)\
             .filterDate(start_date, end_date)\
             .filter(ee.Filter.calendarRange(start_julian, end_julian))\
             .filterBounds(bounds)\
-            .map(ls_sr_band_correction)\
-            .map(ls_sr_only_clear)\
-            .map(add_indices)
-
-    def maxval_comp_ndvi(collection, pctl=50, index='NDVI'):
-        """ function to make pctl th value composite"""
-        index_band = collection.select(index).reduce(ee.Reducer.percentile([pctl]))
-        with_dist = collection.map(lambda(image): image.addBands(image.select(index)
-                                                                 .subtract(index_band).abs().multiply(-1)
-                                                                 .rename('quality')))
-        return with_dist.qualityMosaic('quality')
-
-    def interval_mean(collection, min_pctl, max_pctl, internal_bands):
-        """ function to make interval mean composite"""
-        temp_img = collection.reduce(ee.Reducer.intervalMean(min_pctl, max_pctl))
-        return temp_img.select(ee.List.sequence(0, internal_bands.length().subtract(1)), internal_bands)
+            .map(EEFunc.ls_sr_band_correction)\
+            .map(EEFunc.ls_sr_only_clear)\
+            .map(EEFunc.add_indices)
 
     def make_ls(args):
         """ function to make image collection"""
@@ -157,11 +37,11 @@ if __name__ == '__main__':
         all_images3 = ee.ImageCollection(get_landsat_images(collection, bounds, start_date, end_date,
                                                             start_julian[2], end_julian[2]))
 
-        img_season1 = ee.Image(add_suffix(maxval_comp_ndvi(all_images1, pctl, index).select(bands), '1'))\
+        img_season1 = ee.Image(EEFunc.add_suffix(EEFunc.maxval_comp_ndvi(all_images1, pctl, index).select(bands), '1'))\
             .unmask(unmask_val)
-        img_season2 = ee.Image(add_suffix(maxval_comp_ndvi(all_images2, pctl, index).select(bands), '2'))\
+        img_season2 = ee.Image(EEFunc.add_suffix(EEFunc.maxval_comp_ndvi(all_images2, pctl, index).select(bands), '2'))\
             .unmask(unmask_val)
-        img_season3 = ee.Image(add_suffix(maxval_comp_ndvi(all_images3, pctl, index).select(bands), '3'))\
+        img_season3 = ee.Image(EEFunc.add_suffix(EEFunc.maxval_comp_ndvi(all_images3, pctl, index).select(bands), '3'))\
             .unmask(unmask_val)
 
         slope = ee.Terrain.slope(elevation_image).multiply(elev_scale_factor)
