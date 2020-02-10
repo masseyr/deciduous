193168f01911006eb234492092a2e8b1b84fb012
Richard Massey <rm885@nau.edu>
2020-02-10 00:02:51 -0700

    Update __init__.py
    renamed classification.py to regression.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,6 +1,6 @@
 from common import Sublist, Handler, FTPHandler, Opt, EEFunc, \
     OGR_GEOM_DEF, OGR_TYPE_DEF, OGR_FIELD_DEF, GDAL_FIELD_DEF, OGR_FIELD_DEF_INV, GDAL_FIELD_DEF_INV
-from classification import RFRegressor, MRegressor, HRFRegressor, _Regressor
+from regression import RFRegressor, MRegressor, HRFRegressor, _Regressor
 from samples import Samples
 from raster import Raster, MultiRaster
 from resources import *
