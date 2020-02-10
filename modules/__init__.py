6a1551e68248f12c50254a814ec0e6ad95454630
Richard Massey <rm885@nau.edu>
2020-02-10 00:48:32 -0700

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,9 +1,9 @@
-from common import Sublist, Handler, FTPHandler, Opt, EEFunc, \
-    OGR_GEOM_DEF, OGR_TYPE_DEF, OGR_FIELD_DEF, GDAL_FIELD_DEF, OGR_FIELD_DEF_INV, GDAL_FIELD_DEF_INV
+from common import Sublist, Handler, FTPHandler, Opt
+from gdaldefs import OGR_GEOM_DEF, OGR_TYPE_DEF, OGR_FIELD_DEF, GDAL_FIELD_DEF, OGR_FIELD_DEF_INV, GDAL_FIELD_DEF_INV
+from eelib import EEFunc
 from regression import RFRegressor, MRegressor, HRFRegressor, _Regressor
 from samples import Samples
 from raster import Raster, MultiRaster
-from resources import *
 from timer import Timer
 from distance import Mahalanobis, Distance, Euclidean
 from exceptions import ObjectNotFound, UninitializedError, FieldError, FileNotFound, TileNotFound, ImageProcessingError
