bfa85762e34fd3c81cd5b1412c264093e64c7da9
Richard Massey <rm885@nau.edu>
2020-02-22 23:35:20 -0700

    Update reproj_to_laea_clip.py

--- data/reproj_to_laea_clip.py
+++ data/reproj_to_laea_clip.py
@@ -8,26 +8,29 @@ if __name__ == '__main__':
     # epsg = 3571
     out_proj4 = '+proj=laea +lat_0=90 +lon_0=180 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs '
 
-    outfile = outfolder + Handler(Handler(filename).add_to_filename('_NPLAEA_clipped')).basename
+    tempfile = outfolder + Handler(Handler(filename.replace('EPSG-4326', ''))
+                                   .add_to_filename('_NPLAEA')).basename
+    outfile = outfolder + Handler(Handler(filename.replace('EPSG-4326', ''))
+                                  .add_to_filename('_NPLAEA_clipped')).basename
 
     ras = Raster(filename)
     ras.initialize()
 
     print(ras)
 
-    ras.reproject(outfile=outfile,
+    ras.reproject(outfile=tempfile,
                   out_proj4=out_proj4,
                   output_res=(100, 100),
                   out_nodatavalue=0.0,
                   bigtiff='yes',
-                  cutline_file=cutfile,
                   compress='lzw')
+
+    temp_ras = Raster(tempfile)
+    temp_ras.clip(cutline_file=cutfile,
+                  outfile=outfile)
+
+    Handler(tempfile).file_delete()
 
     Raster(outfile).add_overviews(bigtiff='yes',
                                   compress='lzw')
-
-
-
-
-
 
